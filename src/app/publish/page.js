97966e3d4ce8bd4d18f5f4ab23124bc6136f5893
jdlee <jdlee@jasongroup.co.kr>
2024-02-06 10:34:51 +0900

    publish 페이지 개발중

--- src/app/publish/page.js
+++ src/app/publish/page.js
@@ -1,15 +1,19 @@
 import Link from "next/link";
+import '/public/styles/publish.scss';
 
+export const getServerSideProps = (async () => {
+
+})
 
 export default function Publish() {
-  return (
-    <section className="publish">
-      <ul>
-          <li>
-              <p><Link href="/">대전시통합도서관 작은도서관 도서관리시스템 구축사업</Link></p>
-              <p>메인 및 서브 데코레이터 퍼블리싱</p>
-          </li>
-      </ul>
-    </section>
-  )
+    return (
+        <section className="publish">
+            <ul>
+                <li>
+                    <p><Link href="/">대전시통합도서관 작은도서관 도서관리시스템 구축사업</Link></p>
+                    <p>메인 및 서브 데코레이터 퍼블리싱</p>
+                </li>
+            </ul>
+        </section>
+    )
 }
