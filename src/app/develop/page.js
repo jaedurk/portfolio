8d645eb5937a5bfb07f580f1c4c9b8436076749c
jdlee <jdlee@jasongroup.co.kr>
2024-02-15 16:46:12 +0900

    develop 페이지 개발중

--- src/app/develop/page.js
+++ src/app/develop/page.js
@@ -1,9 +1,28 @@
 
+import '/public/styles/develop.scss';
+import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss";
 
-export default function Develop() {
-  return (
-    <section>
-      Develop
-    </section>
-  )
+export default async function Develop() {
+    const response = await fetch(process.env.NEXT_PUBLIC_API_URL + 'front', {cache: 'no-store'});
+    const developList = await response.json();
+
+    return (
+        <section className="front">
+            <ul>
+                {developList.map((developItem) => {
+                    return <li key={developItem.id}>
+                        <div>
+                            <p>프로젝트명 : {developItem.name}</p>
+                            <p>기간 : {developItem.period}</p>
+                            <p>Front-end :
+                                {developList.skills.map((skillList) => {
+                                    return skillList
+                                })}
+                            </p>
+                        </div>
+                    </li>
+                })}
+            </ul>
+        </section>
+    )
 }
