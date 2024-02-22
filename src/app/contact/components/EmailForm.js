5ebf6a54051f46dbcb91ac77728c7287d48c79db
jdlee <jdlee@jasongroup.co.kr>
2024-02-22 17:57:19 +0900

    Contact 테스트

---
+++ src/app/contact/components/EmailForm.js
@@ -0,0 +1,7 @@
+'use client';
+
+export default function EmailForm() {
+    return (
+        <div className="email-form">폼이 들어갑니다</div>
+    )
+}
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,9 +1,11 @@
 import '/public/styles/contact.scss';
+import EmailForm from "@/app/contact/components/EmailForm";
 
 export default function Contact() {
-  return (
-    <section className="contact">
-      contact
-    </section>
-  )
+    return (
+        <section className="contact">
+            <h2>Contact</h2>
+            <EmailForm/>
+        </section>
+    )
 }
