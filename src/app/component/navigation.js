d7429d4de74f9db7378740467db5eea7ac83cfe0
jdlee <jdlee@jasongroup.co.kr>
2024-02-29 18:23:47 +0900

    navigation 작업 완료

--- src/app/component/navigation.js
+++ src/app/component/navigation.js
@@ -5,10 +5,8 @@ import {usePathname} from "next/navigation";
 
 export default function Navigation(props) {
     const pathName = usePathname();
-    console.log('pathName : ', pathName);
     return props.nav.map((navigationName, navigationIndex) => {
-        console.log('navigationName : ', navigationName);
-        return <li key={navigationIndex} className={'/' + navigationName === pathName ? 'on' : ''}>
+        return <li key={navigationIndex} className={'/' + navigationName === pathName && 'on'}>
             <Link href={navigationName}>{navigationName}</Link>
         </li>
     })
