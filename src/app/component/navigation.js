7e922870d4d857f75c287a0adb8902f394431dbb
jdlee <jdlee@jasongroup.co.kr>
2024-03-07 13:36:22 +0900

    수정

--- src/app/component/navigation.js
+++ src/app/component/navigation.js
@@ -5,7 +5,6 @@ import {usePathname} from "next/navigation";
 
 export default function Navigation(props) {
     const pathName = usePathname();
-    console.log(props, pathName)
     return props.nav.map((navigationName, navigationIndex) => {
         return <li key={navigationIndex} className={`/${navigationName.toLowerCase()}` === pathName ? 'on' : ''}>
             <Link href={navigationName.toLowerCase()}>{navigationName}</Link>
