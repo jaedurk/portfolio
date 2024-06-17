1d327151664fe969fb3d31c26dc1043085594327
jdlee <jdlee@jasongroup.co.kr>
2024-06-17 15:28:08 +0900

    퍼블리싱 반응형웹 여부 추가

--- src/app/api/publish/route.js
+++ src/app/api/publish/route.js
@@ -12,7 +12,7 @@ const publishList = [
     {
         "id": 2,
         "name": "전남도립대학교 전자도서 관리시스템 업그레이드",
-        "detail": "상단메뉴 동적 UI 구현",
+        "detail": "상단메뉴 동적 UI 구현(반응형웹)",
         "url": "http://lib.dorip.ac.kr/",
         "imageUrl": "dorip.jpg",
         "contribution": 100
@@ -20,7 +20,7 @@ const publishList = [
     {
         "id": 3,
         "name": "한국노동연구원 통합전자도서관 시스템 고도화",
-        "detail": "메인 및 서브 데코레이터 퍼블리싱",
+        "detail": "메인 및 서브 데코레이터 퍼블리싱(반응형웹)",
         "url": "http://dl.kli.re.kr/",
         "imageUrl": "nodong.jpg",
         "contribution": 100
@@ -28,7 +28,7 @@ const publishList = [
     {
         "id": 4,
         "name": "통계청 디지털 통계도서관 고도화 사업",
-        "detail": "반응형 전환",
+        "detail": "반응형 전환(반응형웹)",
         "url": "https://lib1.kostat.go.kr/",
         "imageUrl": "kostat.jpg",
         "contribution": 100
@@ -36,7 +36,7 @@ const publishList = [
     {
         "id": 5,
         "name": "국회도서관 학술연구자정보 공유시스템 유지보수",
-        "detail": "기존 페이지 수정 및 추가 페이지 퍼블리싱",
+        "detail": "기존 페이지 수정 및 추가 페이지 퍼블리싱(반응형웹)",
         "url": "https://dl.nanet.go.kr/",
         "imageUrl": "nanet.jpg",
         "contribution": 100
@@ -44,7 +44,7 @@ const publishList = [
     {
         "id": 6,
         "name": "2019년 국가전자도서관 시스템 고도화",
-        "detail": "전 페이지 퍼블리싱 (CSS 및 이미지 구조 설계)",
+        "detail": "전 페이지 퍼블리싱 (CSS 및 이미지 구조 설계, 반응형웹)",
         "url": "https://www.dlibrary.go.kr/",
         "imageUrl": "dlibrary.jpg",
         "contribution": 100
@@ -68,7 +68,7 @@ const publishList = [
     {
         "id": 9,
         "name": "통계청 도서관 전자도서관 홈페이지 디자인 개편",
-        "detail": "메인 및 서브 데코레이터 퍼블리싱",
+        "detail": "메인 및 서브 데코레이터 퍼블리싱(반응형웹)",
         "url": "https://lib1.kostat.go.kr/",
         "imageUrl": "kostat.jpg",
         "contribution": 100
@@ -76,7 +76,7 @@ const publishList = [
     {
         "id": 10,
         "name": "학술연구정보서비스 개방 및 연계",
-        "detail": "기존 페이지 수정 및 추가 페이지 퍼블리싱",
+        "detail": "기존 페이지 수정 및 추가 페이지 퍼블리싱(반응형웹)",
         "url": "http://www.rinfo.kr/",
         "imageUrl": "rinfo.jpg",
         "contribution": 100
@@ -84,7 +84,7 @@ const publishList = [
     {
         "id": 11,
         "name": "한국예술종합학교 반응형 전자도서관 홈페이지 구축",
-        "detail": "메인 및 서브 데코레이터 퍼블리싱",
+        "detail": "메인 및 서브 데코레이터 퍼블리싱(반응형웹)",
         "url": "http://lib.karts.ac.kr/",
         "imageUrl": "karts.jpg",
         "contribution": 100
@@ -92,15 +92,15 @@ const publishList = [
     {
         "id": 12,
         "name": "국회도서관 학술연구자정보 공유시스템 고도화 사업",
-        "detail": "기존 페이지 수정 및 추가 페이지 퍼블리싱",
-        "url": "https://dl.nanet.go.kr/",
+        "detail": "기존 페이지 수정 및 추가 페이지 퍼블리싱(반응형)",
+        "url": "https://dl.nan웹.go.kr/",
         "imageUrl": "nanet.jpg",
         "contribution": 100
     },
     {
         "id": 13,
         "name": "동국대학교 서울경주 첨단도서관 시스템 구축",
-        "detail": "메인 및 서브 데코레이터 퍼블리싱",
+        "detail": "메인 및 서브 데코레이터 퍼블리싱(반응형웹)",
         "url": "http://libk.dongguk.ac.kr/",
         "imageUrl": "",
         "contribution": 100
@@ -108,7 +108,7 @@ const publishList = [
     {
         "id": 14,
         "name": "우즈베키스탄 2016",
-        "detail": "우즈벡 국립 및 지역 도서관 사이트 퍼블리싱",
+        "detail": "우즈벡 국립 및 지역 도서관 사이트 퍼블리싱(반응형웹)",
         "url": "https://www.natlib.uz/",
         "imageUrl": "natlib.png",
         "contribution": 100
@@ -116,7 +116,7 @@ const publishList = [
     {
         "id": 15,
         "name": "대구가톨릭대학교 학술정보관리시스템 개선사업",
-        "detail": "메인 및 서브 데코레이터 퍼블리싱",
+        "detail": "메인 및 서브 데코레이터 퍼블리싱(반응형웹)",
         "url": "https://lib.cu.ac.kr/",
         "imageUrl": "cu.jpg",
         "contribution": 100
@@ -140,7 +140,7 @@ const publishList = [
     {
         "id": 18,
         "name": "인권도서관 인권자료관리시스템 구축",
-        "detail": "메인 및 서브 데코레이터 퍼블리싱\n웹접근성 관련 UI 및 스크립트 수정",
+        "detail": "메인 및 서브 데코레이터 퍼블리싱\n웹접근성 관련 UI 및 스크립트 수정(반응형웹)",
         "url": "https://library.humanrights.go.kr/",
         "imageUrl": "humanRights.jpg",
         "contribution": 100
@@ -148,7 +148,7 @@ const publishList = [
     {
         "id": 19,
         "name": "서울도서관 홈페이지 이용자서비스 고도화 및 디자인 개선",
-        "detail": "메인 및 서브 데코레이터 퍼블리싱\n웹접근성 인증마크 획득",
+        "detail": "메인 및 서브 데코레이터 퍼블리싱\n웹접근성 인증마크 획득(반응형웹)",
         "url": "https://library.humanrights.go.kr/",
         "imageUrl": "seoul.jpg",
         "contribution": 100
@@ -156,7 +156,7 @@ const publishList = [
     {
         "id": 20,
         "name": "안산대학교 도서관 통합학술정보시스템 구축",
-        "detail": "메인 및 서브 데코레이터 퍼블리싱",
+        "detail": "메인 및 서브 데코레이터 퍼블리싱(반응형웹)",
         "url": "https://lib.ansan.ac.kr/",
         "imageUrl": "seoul.jpg",
         "contribution": 100
@@ -164,7 +164,7 @@ const publishList = [
     {
         "id": 21,
         "name": "박정희대통령 기념재단 통합 전자도서관 시스템 구축 사업",
-        "detail": "메인 및 서브 데코레이터 퍼블리싱",
+        "detail": "메인 및 서브 데코레이터 퍼블리싱(반응형웹)",
         "url": "http://www.presidentparkchunghee.org/",
         "imageUrl": "pch.jpg",
         "contribution": 100
@@ -180,7 +180,7 @@ const publishList = [
     {
         "id": 23,
         "name": "우송대학교 도서관 홈페이지 리뉴얼 제작 계약",
-        "detail": "메인 및 서브 데코레이터 퍼블리싱",
+        "detail": "메인 및 서브 데코레이터 퍼블리싱(반응형웹)",
         "url": "http://library.wsu.ac.kr/",
         "imageUrl": "wsu.jpg",
         "contribution": 100
@@ -196,7 +196,7 @@ const publishList = [
     {
         "id": 25,
         "name": "삼육대학교 튤립 업그레이드",
-        "detail": "메인 및 서브 데코레이터 퍼블리싱",
+        "detail": "메인 및 서브 데코레이터 퍼블리싱(반응형웹)",
         "url": "https://lib.syu.ac.kr/",
         "imageUrl": "syu.jpg",
         "contribution": 100
@@ -228,7 +228,7 @@ const publishList = [
     {
         "id": 29,
         "name": "한국정보화진흥원 차세대업무시스템",
-        "detail": "메인 및 서브 데코레이터 퍼블리싱",
+        "detail": "메인 및 서브 데코레이터 퍼블리싱(반응형웹)",
         "url": "http://library.nia.or.kr/",
         "imageUrl": "nia.png",
         "contribution": 100
@@ -236,7 +236,7 @@ const publishList = [
     {
         "id": 30,
         "name": "한국예술종합학교 TULIP 3.0 업그레이드",
-        "detail": "메인 및 서브 데코레이터 퍼블리싱",
+        "detail": "메인 및 서브 데코레이터 퍼블리싱(반응형웹)",
         "url": "https://lib.karts.ac.kr/",
         "imageUrl": "karts.jpg",
         "contribution": 100
@@ -260,7 +260,7 @@ const publishList = [
     {
         "id": 33,
         "name": "청주대학교 중앙도서관 전자도서관업그레이드",
-        "detail": "메인 및 서브 데코레이터 퍼블리싱",
+        "detail": "메인 및 서브 데코레이터 퍼블리싱(반응형웹)",
         "url": "https://library.cju.ac.kr/",
         "imageUrl": "cju.png",
         "contribution": 100
@@ -268,7 +268,7 @@ const publishList = [
     {
         "id": 34,
         "name": "2015년 이화여자대학교 홈페이지 반응형 웹 개편",
-        "detail": "메인 및 서브 데코레이터 퍼블리싱, 모듈 화면 커스터마이징",
+        "detail": "메인 및 서브 데코레이터 퍼블리싱, 모듈 화면 커스터마이징(반응형웹)",
         "url": "http://lib.ewha.ac.kr/",
         "imageUrl": "ewha.jpg",
         "contribution": 100
@@ -284,7 +284,7 @@ const publishList = [
     {
         "id": 36,
         "name": "순천향대학교 통합 전자도서관시스템 구축",
-        "detail": "메인 및 서브 데코레이터 퍼블리싱",
+        "detail": "메인 및 서브 데코레이터 퍼블리싱(반응형웹)",
         "url": "https://library.sch.ac.kr/",
         "imageUrl": "sch.png",
         "contribution": 100
@@ -292,7 +292,7 @@ const publishList = [
     {
         "id": 37,
         "name": "KDISCHOOL 통합도서관자동화시스템구축",
-        "detail": "메인 및 서브 데코레이터 퍼블리싱",
+        "detail": "메인 및 서브 데코레이터 퍼블리싱(반응형웹)",
         "url": "https://library.kdischool.ac.kr/",
         "imageUrl": "kdiSchool.jpg",
         "contribution": 100
@@ -348,7 +348,7 @@ const publishList = [
     {
         "id": 44,
         "name": "연세대학교 통합형 전자도서관 시스템 구축",
-        "detail": "반응형웹 구조 설정 및 전체페이지 퍼블리싱",
+        "detail": "반응형웹 구조 설정 및 전체페이지 퍼블리싱(반응형웹)",
         "url": "https://library.yonsei.ac.kr/",
         "imageUrl": "yonsei.jpg",
         "contribution": 100
@@ -356,7 +356,7 @@ const publishList = [
     {
         "id": 45,
         "name": "가천대학교 도서관 웹기반 통합시스템",
-        "detail": "메인 및 서브 데코레이터 퍼블리싱",
+        "detail": "메인 및 서브 데코레이터 퍼블리싱(반응형웹)",
         "url": "http://lib.gachon.ac.kr/",
         "imageUrl": "gachon.png",
         "contribution": 100
